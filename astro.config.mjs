cb5f5cadc742b42d7fbcfe785267d386c660353c
Hasan Ali <hasanhaja@gmail.com>
2023-02-26 20:40:05 +0000

    Project init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import deno from "@astrojs/deno";
+
+// https://astro.build/config
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: deno(),
+  integrations: [solidJs(), tailwind()]
+});
