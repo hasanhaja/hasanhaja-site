e4853b6d8e495916ae1526472ea10354c00f8d14
Hasan Ali <hasanhaja@gmail.com>
2023-03-18 15:58:59 +0000

    WIP reskining everything

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,47 @@
+const withOpacity = (variableName) => ({ opacityValue }) => {
+  if (opacityValue !== undefined) {
+    return `rgba(var(${variableName}), ${opacityValue})`;
+  }
+
+  return `rgb(var(${variableName}))`;
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      textColor: {
+        skin: {
+          base: withOpacity("--color-base"),
+          inverted: withOpacity("--color-inverted"),
+          accent: withOpacity("--color-accent"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          primary: withOpacity("--color-primary"),
+          secondary: withOpacity("--color-secondary"),
+          inverted: withOpacity("--color-inverted"),
+          section: withOpacity("--color-section"),
+          accent: withOpacity("--color-accent"),
+        },
+      },
+      fill: {
+        skin: {
+          base: withOpacity("--color-base"),
+          inverted: withOpacity("--color-inverted"),
+          accent: withOpacity("--color-accent"),
+        },
+      },
+      stroke: {
+        skin: {
+          base: withOpacity("--color-base"),
+          inverted: withOpacity("--color-inverted"),
+          accent: withOpacity("--color-accent"),
+        },
+      },
+    },
 	},
 	plugins: [],
 }
